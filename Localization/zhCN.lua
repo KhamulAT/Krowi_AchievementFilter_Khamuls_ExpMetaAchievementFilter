48039c2ae0224083af5e579c6bb02e4e6ca72d86
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-11-19 13:24:17 +0800

    Create zhCN.lua

---
+++ Localization/zhCN.lua
@@ -0,0 +1,32 @@
+local addonName, addon = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "zhCN")
+if not L then return end
+addon.L = L;
+
+-- Globals
+L["Khamul's Meta-Expansion Achievement List"] = "Khamul's Meta-资料片新增成就列表"
+L["Unknown Achievement"] = "未知成就"
+
+-- Tooltips
+L["Tt_ACM_15035"] = "只需4个即可完成成就"
+L["Tt_Preview"] = "（预览）许多成就将在 11.0.7 中添加"
+
+-- Not yet available achievements names
+L["ACM_40953"] = "永别了，武装"
+L["ACM_40956"] = "登岛时光"
+L["ACM_41202"] = "真的很赞"
+L["ACM_41205"] = "峡之大者"
+L["ACM_41203"] = "死路顺风"
+L["ACM_41206"] = "风暴之歌"
+L["ACM_41204"] = "沙丘小队"
+L["ACM_41207"] = "尘埃落定"
+L["ACM_40955"] = "战利品"
+L["ACM_40957"] = "全军协力"
+L["ACM_40959"] = "黑暗帝国的心智"
+L["ACM_40958"] = "尽心歇力，百战不殆"
+L["ACM_41209"] = "戎装焕发：“争霸艾泽拉斯”"
+L["ACM_41201"] = "疑影重重"
+L["ACM_41186"] = "金石之盟"
+L["ACM_41187"] = "与主机同心"
+L["ACM_41188"] = "水晶年代录"
+L["ACM_41189"] = "艾基随之转动"
