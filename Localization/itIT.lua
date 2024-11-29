bc647021ecc261f5ebc3966ebf9e7de34787b0db
Khamul/Nyielle/Saraphîna - Antonidas <neytiri-amanthul@gmx.at>
2024-11-29 12:07:18 +0100

    Re-added the missing achievements locale data, so beta should also work on retail servers

--- Localization/itIT.lua
+++ Localization/itIT.lua
@@ -11,3 +11,23 @@ L["Unknown Achievement"] = "Realizzazione sconosciuta"
 
 -- Tooltips
 L["Tt_ACM_15035"] = "Ne servono solo 4 per completare il Meta-Raggiungimento"
+
+-- Not yet available achievements names (no longer needed when 11.0.7 hit the live servers)
+L["ACM_40953"] = "A Farewell to Arms"
+L["ACM_40956"] = "I'm On Island Time"
+L["ACM_41202"] = "Hot Tropic"
+L["ACM_41205"] = "Sound Off"
+L["ACM_41203"] = "Bwon Voyage"
+L["ACM_41206"] = "Songs of Storms"
+L["ACM_41204"] = "Dune Squad"
+L["ACM_41207"] = "When the Drust Settles"
+L["ACM_40955"] = "War Stories"
+L["ACM_40957"] = "Maximum Effort"
+L["ACM_40959"] = "Black Empire State of Mind"
+L["ACM_40958"] = "Full Heart, Can't Lose"
+L["ACM_41209"] = "Dressed to Kill: Battle for Azeroth"
+L["ACM_41201"] = "You Xal Not Pass"
+L["ACM_41186"] = "Slate of the Union"
+L["ACM_41187"] = "Rage Aside the Machine"
+L["ACM_41188"] = "Crystal Chronicled"
+L["ACM_41189"] = "Azj the World Turns"
