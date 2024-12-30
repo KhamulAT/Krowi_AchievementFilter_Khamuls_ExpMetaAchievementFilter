8262fd83aec9bfbcd9661e139eba85b43299fe14
Khamul/Nyielle/Saraphîna - Antonidas <neytiri-amanthul@gmx.at>
2024-12-30 12:44:49 +0100

    Initialize Addon when player login event has fired
    Some achievement data (like completition state) is available AFTER the player login event has been fired ... so the Dressed to Kill achievements did include all achievements on first login, instead of only the completed (if the requirement is fullfilled)

--- Krowi_AchievementFilter_Khamuls_ExpMetaAchievementFilter.lua
+++ Krowi_AchievementFilter_Khamuls_ExpMetaAchievementFilter.lua
@@ -7,22 +7,45 @@ if not KrowiAF then
     return -- Exit the script
 end
 
--- add all meta achievements to 
-local aggregatedAchievementsList = {
-    971,
-    {
-        addon.L["Khamul's Meta-Expansion Achievement List"],
-        addon.Achievements.BfA,
-        addon.Achievements.SL,
-        addon.Achievements.DF,
-        addon.Achievements.TWW,
+local KhamulsEventListenerFrame = CreateFrame("Frame")
+function KhamulsEventListenerFrame:Disable() 
+    self:UnregisterAllEvents()
+    self:SetScript("OnEvent", nil)
+    self:SetScript("OnUpdate", nil)
+    self:Hide()
+end
+
+KhamulsEventListenerFrame:RegisterEvent("PLAYER_LOGIN")
+KhamulsEventListenerFrame:SetScript("OnEvent", function(self, event, ...)
+    if event == "PLAYER_LOGIN" then
+        InitializeAllAchievementsForKhamulsMetaList()
+        KhamulsEventListenerFrame:Disable()
+    end
+end)
+
+function InitializeAllAchievementsForKhamulsMetaList() 
+    -- add all meta achievements to 
+    InitializeBfA()
+    InitializeSL()
+    InitializeDF()
+    InitializeTWW()
+
+    local aggregatedAchievementsList = {
+        971,
         {
-            addon.Achievements.BfAMetaAchievementId,
-            addon.Achievements.SLMetaAchievementId,
-            addon.Achievements.DFMetaAchievementId,
-            addon.Achievements.TWWMetaAchievementId
+            addon.L["Khamul's Meta-Expansion Achievement List"],
+            addon.Achievements.BfA,
+            addon.Achievements.SL,
+            addon.Achievements.DF,
+            addon.Achievements.TWW,
+            {
+                addon.Achievements.BfAMetaAchievementId,
+                addon.Achievements.SLMetaAchievementId,
+                addon.Achievements.DFMetaAchievementId,
+                addon.Achievements.TWWMetaAchievementId
+            }
         }
     }
-}
 
-KrowiAF.CategoryData.KhamulsExpansionMetaAchievementLists = aggregatedAchievementsList
+    KrowiAF.CategoryData.KhamulsExpansionMetaAchievementLists = aggregatedAchievementsList
+end
